b69817b5ddc7e921a347389e924ba5724ccf2f51
tthdvd88 <tthdvd88@gmail.com>
2022-11-28 12:11:59 +0100

    Film.java létrehozva az SQL kapcsolathoz

--- src/main/java/com/example/moziapp/Film.java
+++ src/main/java/com/example/moziapp/Film.java
@@ -1,5 +1,12 @@
 package com.example.moziapp;
 
 public class Film {
+    private Integer fkod;
+    private String filmcim;
+    private boolean szines;
+    private String szinkron;
+    private String szarmazas;
+    private String mufaj;
+    private Integer hossz;
 
 }
