8d7189635f531138d3246e4c5d80fbc32065c17a
tthdvd88 <tthdvd88@gmail.com>
2022-11-28 12:26:42 +0100

    Maincontroller > SampleSkeleton from Scenebuilder

--- src/main/java/com/example/moziapp/Film.java
+++ src/main/java/com/example/moziapp/Film.java
@@ -1,6 +1,72 @@
 package com.example.moziapp;
 
 public class Film {
+    public Integer getFkod() {
+        return fkod;
+    }
+
+    public void setFkod(Integer fkod) {
+        this.fkod = fkod;
+    }
+
+    public String getFilmcim() {
+        return filmcim;
+    }
+
+    public void setFilmcim(String filmcim) {
+        this.filmcim = filmcim;
+    }
+
+    public boolean isSzines() {
+        return szines;
+    }
+
+    public void setSzines(boolean szines) {
+        this.szines = szines;
+    }
+
+    public String getSzinkron() {
+        return szinkron;
+    }
+
+    public void setSzinkron(String szinkron) {
+        this.szinkron = szinkron;
+    }
+
+    public String getSzarmazas() {
+        return szarmazas;
+    }
+
+    public void setSzarmazas(String szarmazas) {
+        this.szarmazas = szarmazas;
+    }
+
+    public String getMufaj() {
+        return mufaj;
+    }
+
+    public void setMufaj(String mufaj) {
+        this.mufaj = mufaj;
+    }
+
+    public Integer getHossz() {
+        return hossz;
+    }
+
+    public void setHossz(Integer hossz) {
+        this.hossz = hossz;
+    }
+
+    public Film(Integer fkod, String filmcim, boolean szines, String szinkron, String szarmazas, String mufaj, Integer hossz) {
+        this.fkod = fkod;
+        this.filmcim = filmcim;
+        this.szines = szines;
+        this.szinkron = szinkron;
+        this.szarmazas = szarmazas;
+        this.mufaj = mufaj;
+        this.hossz = hossz;
+    }
+
     private Integer fkod;
     private String filmcim;
     private boolean szines;
--- src/main/java/com/example/moziapp/MoziappController.java
+++ src/main/java/com/example/moziapp/MoziappController.java
@@ -1,17 +1,75 @@
 package com.example.moziapp;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-public class MoziappController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoziappController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Üdvözöllek pici hobbit");
+    private Button btnDelete;
+
+    @FXML
+    private Button btnInsert;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colFilmcim;
+
+    @FXML
+    private TableColumn<?, ?> colFilmk;
+
+    @FXML
+    private TableColumn<?, ?> colHossz;
+
+    @FXML
+    private TableColumn<?, ?> colMufaj;
+
+    @FXML
+    private TableColumn<?, ?> colSzarmazas;
+
+    @FXML
+    private TableColumn<?, ?> colSzines;
+
+    @FXML
+    private TableColumn<?, ?> colSzinkron;
+
+    @FXML
+    private TextField tfFcim;
+
+    @FXML
+    private TextField tfFkod;
+
+    @FXML
+    private TextField tfHossz;
+
+    @FXML
+    private TextField tfMufaj;
+
+    @FXML
+    private TextField tfSzarmazas;
+
+    @FXML
+    private TextField tfSzines;
+
+    @FXML
+    private TextField tfSzinkron;
+
+    @FXML
+    private TableView<?> tvFilm;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
-
-    @FXML
-    protected void exitApplicationButton() {}
 }
+
